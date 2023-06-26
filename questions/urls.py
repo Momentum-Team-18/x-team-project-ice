d57ac186994e5f6619cab421741a4df06a0b0305
Steve Lehew <120685948+SLehew@users.noreply.github.com>
2023-06-26 11:44:17 -0400

    added pk to answer list url

--- questions/urls.py
+++ questions/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path("answers/accept/<int:pk>/",
          views.AcceptAnswerViewSet.as_view(), name="accept_answer"),
     # GET
-    path("answers/user/",
+    path("answers/user/<int:pk>/",
          views.UserAnswersViewSet.as_view(), name="user_answers"),
     # DELETE
     path("questions/delete/<int:pk>/",
