b892661aa3474743c3fb91cef6e2970f44f817a5
Steve Lehew <120685948+SLehew@users.noreply.github.com>
2023-06-27 16:24:01 -0400

    Revert "added upload model with question and answer attachments"

--- questions/admin.py
+++ questions/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from questions.models import User, Question, Answer, Upload
+from questions.models import User, Question, Answer
 from django.contrib.auth.admin import UserAdmin
 
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Question)
 admin.site.register(Answer)
-admin.site.register(Upload)
